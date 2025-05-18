1a13f7b5f0c49a4d4a3659a909099ef5ea8d65d5
Masoud Jami <masoud.jami@qt.io>
2025-05-18 17:07:15 +0200

    add license to source code

--- src/square.h
+++ src/square.h
@@ -1,3 +1,25 @@
+// MIT License
+//
+// Copyright (c) 2025 Masoud Jami
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #ifndef SQUARE_H
 #define SQUARE_H
 
@@ -23,41 +45,19 @@ public:
 
     int row() const noexcept {return m_row;}
     int col() const noexcept {return m_col;}
-    QChar piece() const noexcept {return m_piece;}
-    void setPiece(const QChar &piece)
-    {
-        m_piece = piece;
-        emit pieceChanged();
-    }
 
-    void setPieceQuitely(const QChar &piece)
-    {
-        m_piece = piece;
-    }
+    QChar piece() const noexcept {return m_piece;}
+    void setPiece(const QChar &piece);
+    void setPieceQuitely(const QChar &piece) { m_piece = piece;}
 
     bool isLegalDestination() const { return m_isLegalDestination; }
-    void setLegalDestination(bool value) {
-        if (m_isLegalDestination != value) {
-            m_isLegalDestination = value;
-            emit legalDestinationChanged();
-        }
-    }
+    void setLegalDestination(bool value);
 
     bool highlight() const { return m_highlight; }
-    void setHighlight(bool value) {
-        if (m_highlight != value) {
-            m_highlight = value;
-            emit highlightChanged();
-        }
-    }
+    void setHighlight(bool value);
 
     bool isSelected() const { return m_isSelected; }
-    void setIsSelected(bool value) {
-        if (m_isSelected != value) {
-            m_isSelected = value;
-            emit isSelectedChanged();
-        }
-    }
+    void setIsSelected(bool value);
 
 signals:
     void pieceChanged();
